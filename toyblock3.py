b4d291329145d04d5d036e32810b0f9cae9baee2
Oscar Triano dotoscat <user@example.com>
2017-10-02 18:19:29 +0200

    Create System with a decorator with parameters

--- toyblock3.py
+++ toyblock3.py
@@ -2,7 +2,29 @@ class Entity:
     pass
 
 class System:
-    pass
+    def __init__(self, attrib):
+        self._attrib = attrib
+        self._callable_ = None
+    
+    @property
+    def callable(self):
+        return self._callable_
+        
+    @callable.setter
+    def callable(self, callable_):
+        if self._callable_ is None:
+            self._callable_ = callable_
+    
+    @property
+    def attrib(self):
+        return self._attrib
+    
+    def __call__(self, *args, **kwargs):
+        return self._callable_(*args, **kwargs)
 
 def system(attributes):
-    pass
+    def _build_system(callable_):
+        new_system = System(attributes)
+        new_system.callable = callable_
+        return new_system
+    return _build_system
