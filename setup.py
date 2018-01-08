b62582399cb59dad539c66588f01d075f68a10fd
Oscar Triano dotoscat <user@example.com>
2018-01-08 18:36:04 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='toyblock3',
+    version='1.0.0',
+    description='A pythonic and Entity-Component-System written in pure Python',
+    url='https://github.com/dotoscat/toyblock3',
+    author='Oscar \'dotoscat\' Triano',
+    license='LGPL-3.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)'
+    ],
+    keywords='development videogame gamedev',
+    packages=['toyblock']
+)
